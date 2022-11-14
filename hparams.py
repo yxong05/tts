7341a63277751b624a74bcdcd0b96fb539c13ff7
yxong05 <76830907+yxong05@users.noreply.github.com>
2022-11-14 22:34:17 +0800

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -5,6 +5,7 @@ import tf_slim as slim
 
 # Default hyperparameters:
 hparams = tf.contrib.slim.training.HParams(
+    # HELLO
     # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
     # text, you may want to use "basic_cleaners" or "transliteration_cleaners" See TRAINING_DATA.md.
     cleaners='english_cleaners',
